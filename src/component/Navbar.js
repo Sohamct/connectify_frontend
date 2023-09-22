e94b0ed59d501ac225817653a950f7c888865a9d
Soham <sohamtejani699@gmail.com>
2023-09-22 17:58:47 +0530

    init

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,17 +1,55 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import './style.css';
+import axios from 'axios';
 
 export function Navbar(props) {
     const navigate = useNavigate();
+    const [userName, setUserName] = useState(null);
 
     const handleLogout = async () => {
         // Clear user data and token
-        console.log('Logging out...');
+        console.log('Logging out');
         localStorage.removeItem('token');
-        props.getUser();
+        getUser();
         navigate('/login'); // Redirect to the login page
-    };    
+    };
+    
+    
+const getUser = async () => {
+    console.log('getUser function called');
+    try {
+        const host = 'http://localhost:5500';
+        const token = localStorage.getItem('token');
+  
+        if (token) {
+            const res = await axios.get(`${host}/api/auth/getUser`, {
+                headers: {
+                    'auth-token': token,
+                },
+            });
+           console.log(res.data.status);
+           if (res.data.status) {
+            console.log('User data received:', res.data.userName); // Add this line
+            setUserName(res.data.userName);
+        } else {
+            console.log('User data not available'); // Add this line
+            setUserName(null);
+        }
+        
+        }else{
+          setUserName(null)
+        }
+        
+    } catch (error) {
+        setUserName(null)
+        console.error(error);
+    }
+  };
+  useEffect(()=>{
+    getUser();
+  })
 
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -38,7 +76,7 @@ export function Navbar(props) {
                                 Home
                             </Link>
                         </li>
-                        {props.userName && (
+                        { userName && (
                             <>
                                 <li className="nav-item">
                                     <Link className="nav-link active" to="/myPost">
@@ -72,7 +110,7 @@ export function Navbar(props) {
                             </button>
                         </form>
 
-                        {!props.userName ? (
+                        {! userName ? (
                             <form className="d-flex" role="search">
                                 <Link className="btn btn-primary mx-2" to="/login" role="button">
                                     Login
@@ -83,7 +121,7 @@ export function Navbar(props) {
                             </form>
                         ) : (
                             <div className="d-flex align-items-center">
-                                <p className="nav-text mx-2">Logged in as: {props.userName}</p>
+                                <p className="nav-text mx-2">Logged in as: { userName}</p>
                                 <button className="btn btn-primary" onClick={handleLogout}>
                                     Logout
                                 </button>
